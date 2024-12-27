8ffbd14c0947a7f29e81e46f925c25006f0d8430
dahhyeon <dahhyeon22@gmail.com>
2024-12-27 23:23:37 +0900

    feat: #1 진입화면 로고 및 텍스트 추가

---
+++ src/pages/SignUpPage/IntroPage.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import mainLogo from '../../assets/images/EatMate_main_Logo.svg';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const IntroPage: React.FC = () => {
+  return (
+    <Container>
+      <img src={mainLogo} alt="eatMate-logo" />
+      <div>혼자보다 함께</div>
+      <div>혼밥도,혼술도,배달비도 걱정 끝! </div>
+      <div>가천대생을 위한 안전하고 편리한 모임 시작해보세요.</div>
+    </Container>
+  );
+};
+export default IntroPage;
