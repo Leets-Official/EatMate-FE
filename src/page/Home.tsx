836edce7218581f05022aacb9103c0e302f5d923
dahhyeon <dahhyeon22@gmail.com>
2024-12-27 16:44:01 +0900

    chore:폰트 test 코드 제거

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,16 +1,8 @@
 import styled from 'styled-components';
 import theme from '../styles/theme';
 
-const Container = styled.div`
-  /* font-family: ${theme.font.pretendard}; */
-`;
 const Home = () => {
-  return (
-    <div>
-      <Container>홈페이지</Container>
-      <div>홈페이지</div>
-    </div>
-  );
+  return <div>홈페이지</div>;
 };
 
 export default Home;
