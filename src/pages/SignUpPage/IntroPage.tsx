2fd11958ee246771459c30efcb38b25f2f73a14e
dahhyeon <dahhyeon22@gmail.com>
2024-12-28 18:50:04 +0900

    refactor:#1 요소 위치 정렬 수정

--- src/pages/SignUpPage/IntroPage.tsx
+++ src/pages/SignUpPage/IntroPage.tsx
@@ -5,6 +5,7 @@ import GoogleIcon from '../../assets/images/GoogleIcon.svg';
 
 export const Container = styled.div`
   min-height: 100vh;
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -16,6 +17,7 @@ const MainText = styled.div`
   font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
   font-size: 24px;
   margin-top: 120px;
+  flex: 1;
 `;
 
 const SubText = styled.div`
@@ -24,21 +26,40 @@ const SubText = styled.div`
   margin-top: 15px;
 `;
 
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 20px;
+  padding: 20px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
 const IntroPage: React.FC = () => {
   return (
     <Container>
-      <div>
-        <img src={mainLogo} alt="eatMate-logo" />
-      </div>
-      <MainText>혼자보다 함께</MainText>
-      <SubText>
-        <div>혼밥도,혼술도,배달비도 걱정 끝!</div>
-        <div>가천대생을 위한 안전하고 편리한 모임 시작해보세요.</div>
-      </SubText>
-      <Button variant="primary-outline" size="lg" color="black" rounded="md">
-        <img src={GoogleIcon} alt="google-icon" />
-        Google로 가입
-      </Button>
+      <ContentWrapper>
+        <div>
+          <img src={mainLogo} alt="eatMate-logo" />
+        </div>
+        <MainText>혼자보다 함께</MainText>
+        <SubText>
+          <div>혼밥도,혼술도,배달비도 걱정 끝!</div>
+          <div>가천대생을 위한 안전하고 편리한 모임 시작해보세요.</div>
+        </SubText>
+      </ContentWrapper>
+      <ButtonWrapper>
+        <Button variant="primary-outline" size="lg" color="black" rounded="md">
+          <img src={GoogleIcon} alt="google-icon" />
+          Google로 가입
+        </Button>
+      </ButtonWrapper>
     </Container>
   );
 };
