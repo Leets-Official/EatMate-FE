038d4736fa9c5caf6698ae6356a5126eaae519ae
dahhyeon <dahhyeon22@gmail.com>
2024-12-28 23:56:48 +0900

    refactor: #1 데이터를 하나의 atom으로 관리하도록 수정

--- src/recoil/atoms/userAtom.tsx
+++ src/recoil/atoms/userAtom.tsx
@@ -1,36 +1,16 @@
 import { atom } from 'recoil';
 
-export const birthdayState = atom<string>({
-  key: 'birthdayState',
-  default: '',
-});
-
-export const genderState = atom<'남성' | '여성' | ''>({
-  key: 'genderState',
-  default: '',
-});
-
-export const phoneNumberState = atom<string>({
-  key: 'phoneNumberState',
-  default: '',
-});
-
-export const mbtiState = atom<string>({
-  key: 'mbtiState',
-  default: '',
-});
-
-export const nicknameState = atom<string>({
-  key: 'nicknameState',
-  default: '',
-});
-
-export const studentIdState = atom<string>({
-  key: 'studentIdState',
-  default: '',
-});
-
-export const profileImgState = atom<File | null>({
-  key: 'profileImgState',
-  default: null,
+export const signupAtom = atom({
+  key: 'signupAtom',
+  default: {
+    year: '',
+    month: '',
+    day: '',
+    gender: '',
+    phoneNumber: '',
+    mbti: '',
+    nickname: '',
+    studentId: '',
+    profilePhoto: null,
+  },
 });
