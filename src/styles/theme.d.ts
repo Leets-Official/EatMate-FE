568c864b4397facf8f4f91f114df56600ba360ed
dahhyeon <dahhyeon22@gmail.com>
2024-12-28 17:20:39 +0900

    refactor:#1 theme.d.ts 수정

--- src/styles/theme.d.ts
+++ src/styles/theme.d.ts
@@ -1,17 +1,33 @@
 declare module 'src/styles/theme' {
   interface Theme {
-    color: {
-      main: string;
-      yellow: string;
-      red: string;
-      orange: string;
-      ivory: string;
-      purple: string;
-      black: string;
+    COLORS: {
+      primary: {
+        main: string;
+        orange: {
+          100: string;
+          200: string;
+        };
+        yellow: string;
+        red: string;
+      };
+      secondary: {
+        ivory: string;
+        purple: string;
+        black: string;
+      };
     };
-    font: {
+
+    FONT_FAMILY: {
       pretendard: string;
       SDG: string;
+    };
+
+    FONT_WEIGHT: {
+      light: number;
+      regular: number;
+      semiBold: number;
+      bold: number;
+      extraBold: number;
     };
   }
 
