950f0c8a866dbe525bfc31effa2f23ad5b6bf003
dahhyeon <dahhyeon22@gmail.com>
2024-12-30 00:37:14 +0900

    refactor:#1 theme style colors에서 primary,secondary 제거

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,23 +1,19 @@
 const COLORS = {
-  primary: {
-    main: '#EB5916', // 메인 색상
-    orange: {
-      100: '#F8B116', // 채도가 더 낮은 주황색
-      200: '#FF9D01', //        더 높은 주황색
-    },
-    yellow: '#F5F378', // 밝은 노란색
-    red: '#FE4600', // 밝은 빨간색
-    gray: {
-      100: '#D4D4D4',
-      200: '#D9D9D9',
-      300: '#ACACAC',
-    },
+  main: '#EB5916', // 메인 색상
+  orange: {
+    100: '#F8B116', // 채도가 더 낮은 주황색
+    200: '#FF9D01', //        더 높은 주황색
   },
-  secondary: {
-    ivory: '#FAF2DF', // 밝은 아이보리
-    purple: '#DCC1FF', // 연한 보라색
-    black: '#1A1A1A', // 검정색
+  yellow: '#F5F378', // 밝은 노란색
+  red: '#FE4600', // 밝은 빨간색
+  gray: {
+    100: '#D4D4D4',
+    200: '#D9D9D9',
+    300: '#ACACAC',
   },
+  ivory: '#FAF2DF', // 밝은 아이보리
+  purple: '#DCC1FF', // 연한 보라색
+  black: '#1A1A1A', // 검정색
   error: '#FF6D6D',
 };
 
