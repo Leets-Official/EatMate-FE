41f91035d47c3e1093d0f6cd79798534e9c7327a
dahhyeon <dahhyeon22@gmail.com>
2024-12-29 18:12:08 +0900

    refactor:#1 스토리 내용 수정

--- src/components/common/Header/Header.stories.tsx
+++ src/components/common/Header/Header.stories.tsx
@@ -1,29 +1,29 @@
 import { Meta, StoryObj } from '@storybook/react';
 import Header from './Header';
-import mainLogo from '../../../assets/images/EatMate_main_Logo.svg';
 
 const meta: Meta<typeof Header> = {
   title: 'Components/Header',
   component: Header,
+  tags: ['autodocs'],
   parameters: {
     layout: 'fullscreen',
   },
   argTypes: {
     title: {
       control: 'text',
-      description: '헤더 중앙에 표시할 텍스트. 로고와 상호 배타적.',
+      description: '헤더 중앙에 표시할 텍스트입니다. (로고와 상호 배타적)',
     },
     showBackButton: {
       control: 'boolean',
-      description: '뒤로가기 버튼 표시 여부',
+      description: '뒤로가기 버튼 표시 여부를 설정합니다.',
     },
     subText: {
       control: 'text',
-      description: '헤더 하단에 표시할 서브 텍스트',
+      description: '헤더 하단에 표시할 서브 텍스트를 설정합니다.',
     },
     onBackClick: {
       action: 'clicked',
-      description: '뒤로가기 버튼 클릭 시 실행될 함수',
+      description: '뒤로가기 버튼 클릭 시 실행될 함수를 설정합니다.',
     },
   },
 };
@@ -52,14 +52,6 @@ export const WithSubText: Story = {
   args: {
     title: undefined,
     showBackButton: false,
-    subText: '배달팟 실시간 모집 중',
-  },
-};
-
-export const WithTitleAndSubText: Story = {
-  args: {
-    title: '모임 만들기',
-    showBackButton: true,
     subText: '배달팟 실시간 모집 중',
   },
 };
