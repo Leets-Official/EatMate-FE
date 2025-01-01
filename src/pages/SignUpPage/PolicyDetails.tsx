4105e246bbab5b3ddb08ca3593243e93df186fbe
dahhyeon <dahhyeon22@gmail.com>
2025-01-01 21:48:14 +0900

    feat:#5 뒤로가기 클릭 시 이전 페이지로 돌아가도록 추가

--- src/pages/SignUpPage/PolicyDetails.tsx
+++ src/pages/SignUpPage/PolicyDetails.tsx
@@ -1,15 +1,24 @@
 import Header from '@/components/common/Header/Header';
 import { policyContents } from '@/constants/policycontents';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const PolicyDetails: React.FC = () => {
+  const nav = useNavigate();
+
+  const onClickToBack = () => {
+    nav(-1);
+  };
   const { termId } = useParams<{ termId: string }>();
   const term = policyContents.find(
     (content) => content.id.toString() === termId
   );
   return (
     <div>
-      <Header showBackButton={true} title="서비스 이용 약관" />
+      <Header
+        showBackButton={true}
+        title="서비스 이용 약관"
+        onBackClick={onClickToBack}
+      />
       <div>{term?.title}</div>
       <div>{term?.content}</div>
     </div>
