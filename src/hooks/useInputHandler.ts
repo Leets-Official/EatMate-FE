cb56ad905ea0843e3c29157b7bdee48d588bf708
dahhyeon <dahhyeon22@gmail.com>
2025-01-05 15:45:51 +0900

    feat:#5 useInputHandler 커스텀 훅 생성

--- src/hooks/useInputHandler.ts
+++ src/hooks/useInputHandler.ts
@@ -1,13 +1,42 @@
 import { useState } from 'react';
+import { useRecoilState } from 'recoil';
 
-const useInputHandler = (initialValue = '') => {
-  const [value, setValue] = useState(initialValue);
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
-
-  return { value, onChange, setValue };
+type UseInputHandlerProps<T> = {
+  atom: any; // recoil atom
+  validate?: (key: keyof T, value: string) => string | null; //유효성 검사 함수
 };
 
-export default useInputHandler;
+export const UseInputHandler = <T extends Record<string, any>>({
+  atom,
+  validate,
+}: UseInputHandlerProps<T>) => {
+  const [state, setState] = useRecoilState<T>(atom);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const handleInputChange = (key: keyof T, value: string) => {
+    setState((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
+
+    if (validate) {
+      const validationError = validate(key, value);
+      setErrorMessage(validationError || '');
+    }
+  };
+
+  const isFormValid = () => {
+    if (!validate) return true;
+    return Object.keys(state).every(
+      (key) => validate(key as keyof T, state[key as keyof T]) === null
+    );
+  };
+
+  return {
+    state,
+    errorMessage,
+    handleInputChange,
+    isFormValid,
+    setErrorMessage,
+  };
+};
