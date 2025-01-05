aa26db0fefef49154e2b2c98e4fb853059ec908a
dahhyeon <dahhyeon22@gmail.com>
2025-01-05 19:58:24 +0900

    fix:#5경로 오타 수정

--- src/pages/SignUpPage/SignUpLayout.tsx
+++ src/pages/SignUpPage/SignUpLayout.tsx
@@ -1,6 +1,6 @@
 import Header from '@/components/common/Header/Header';
 import ProgressBar from '@/components/SignUp/ProgressBar';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 const stepPaths = [
   '/signup/birthday-gender',
@@ -8,18 +8,26 @@ const stepPaths = [
   '/signup/mbti',
   '/signup/nickname',
   '/signup/student-id',
-  '/signup/profile-image',
+  '/signup/profile-img',
 ];
 
 const SignupLayout: React.FC = () => {
   const location = useLocation();
-
+  const nav = useNavigate();
   const currentStepIndex = stepPaths.indexOf(location.pathname);
   const progress = ((currentStepIndex + 1) / stepPaths.length) * 100;
 
+  const handleBackClick = () => {
+    if (location.pathname === '/signup/birthday-gender') {
+      nav('/');
+    } else if (currentStepIndex > 0) {
+      nav(stepPaths[currentStepIndex - 1]);
+    }
+  };
+
   return (
     <div>
-      <Header showBackButton={true} onBackClick={() => window.history.back()} />
+      <Header showBackButton={true} onBackClick={handleBackClick} />
 
       <ProgressBar progress={progress} />
 
