53e048ed21080b514e9c7745c901b5347d8c5a30
dahhyeon <dahhyeon22@gmail.com>
2025-01-11 23:53:00 +0900

    refactor:api 요청으로 사용자 정보를 가져온 후 role값에 따라 페이지 이동하도록 수정

--- src/pages/SignUpPage/LoginCallback.tsx
+++ src/pages/SignUpPage/LoginCallback.tsx
@@ -1,46 +1,24 @@
-import { jwtDecode } from 'jwt-decode';
+import axiosInstance from '@/apis/axiosInstance';
 import { useEffect } from 'react';
-import { useCookies } from 'react-cookie';
 import { useNavigate } from 'react-router-dom';
 
-interface TokenPayload {
-  email: string;
-  role: string;
-  exp: number;
-}
-
 const LoginCallback: React.FC = () => {
-  const [cookies] = useCookies(['accessToken', 'refreshToken']);
   const nav = useNavigate();
 
   useEffect(() => {
     const handleLoginResponse = async () => {
       try {
-        const accessToken = cookies.accessToken;
-        const refreshToken = cookies.refreshToken;
-
-        if (!accessToken) {
-          console.error('Access Token이 없습니다.');
-          return;
-        }
-
-        // Access Token 디코딩
-        const decoded = jwtDecode<TokenPayload>(accessToken);
-        console.log('디코딩된 JWT : ', decoded);
-
-        // // 토큰 로컬스토리지에 저장
-        // localStorage.setItem('accessToken', accessToken);
-        // if (refreshToken) {
-        //   localStorage.setItem('refreshToken', refreshToken);
-        // }
+        const response = await axiosInstance.post('/api/auth/info');
+        console.log('API 응답: ', response.data);
+        const { role } = response.data.result;
 
         // Role에 따라 페이지 이동
-        if (decoded.role === 'USER') {
+        if (role === 'USER') {
           nav('/home');
-        } else if (decoded.role === 'GUEST') {
+        } else if (role === 'GUEST') {
           nav('/signup');
         } else {
-          console.error('알 수 없는 사용자 role: ', decoded.role);
+          console.error('알 수 없는 사용자 role: ', role);
           nav('/intro');
         }
       } catch (error) {
@@ -49,7 +27,7 @@ const LoginCallback: React.FC = () => {
     };
 
     handleLoginResponse();
-  }, [cookies, nav]);
+  }, [nav]);
 
   return <div>로그인 처리 중...</div>;
 };
