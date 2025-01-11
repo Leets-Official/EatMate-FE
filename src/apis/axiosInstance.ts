6b41cf8ae3c4c682862b4f882be816685d180560
dahhyeon <dahhyeon22@gmail.com>
2025-01-12 00:32:59 +0900

    feat: #8 axios instance에 timeout값 추가

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -3,16 +3,10 @@ import axios from 'axios';
 const defaultInstance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
+  timeout: 3000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
-
-defaultInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    if (error.response?.status === 401) {
-      console.error('Error: 401 -인증 실패');
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default defaultInstance;
