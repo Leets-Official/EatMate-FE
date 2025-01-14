7a1aa1774ddb84d4616251a9f621fb1a4b25a6fb
dahhyeon <dahhyeon22@gmail.com>
2025-01-14 20:04:50 +0900

    refactor:#8 필요없는 console log 제거

--- src/pages/SignUpPage/LoginCallback.tsx
+++ src/pages/SignUpPage/LoginCallback.tsx
@@ -8,7 +8,6 @@ const LoginCallback: React.FC = () => {
   useEffect(() => {
     const handleLoginResponse = async () => {
       try {
-        console.log(defaultInstance.defaults.baseURL);
         const response = await defaultInstance.get('/api/auth/info');
         console.log('API 응답: ', response.data);
         const { role } = response.data.result;
