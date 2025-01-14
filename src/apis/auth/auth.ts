0f3936a748a7ce2b37302de84623cb8383a5ef23
dahhyeon <dahhyeon22@gmail.com>
2025-01-14 18:41:33 +0900

    feat:#8 회원가입 api 연결 코드 추가

--- src/apis/auth/auth.ts
+++ src/apis/auth/auth.ts
@@ -1,3 +1,20 @@
 import defaultInstance from '@/apis/axiosInstance';
 
-const response = await defaultInstance.post('/api/auth/signup');
+export const signupUser = async (signupData: {
+  year: number;
+  month: number;
+  day: number;
+  gender: string;
+  phoneNumber: string;
+  mbti: string;
+  studentNumber: number;
+  nickname: string;
+}) => {
+  try {
+    const response = await defaultInstance.post('/api/auth/signup', signupData);
+    return response.data;
+  } catch (error) {
+    console.error('회원가입 요청 중 오류가 발생했습니다.', error);
+    throw error;
+  }
+};
