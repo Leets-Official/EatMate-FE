10a7c5fb46456e4853a90795b8163f2bfbd85471
JIN921 <wlslwlsl808@gmail.com>
2025-01-17 20:14:55 +0900

    feat: 상세페이지 탭 추가

--- src/pages/MeetingDetail.tsx
+++ src/pages/MeetingDetail.tsx
@@ -1,5 +1,33 @@
+import Tabs from '@/components/common/Tab/Tab';
+import { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 390px;
+  position: relative;
+  padding-top: 25px;
+`;
 const MeetingDetail = () => {
-  return <div>홈페이지</div>;
+  const [selectedTabId, setSelectedTabId] = useState('tab1');
+
+  const tabs = [
+    { id: 'tab1', label: '홈' },
+    { id: 'tab2', label: '채팅팅' },
+  ];
+
+  const handleTabClick = (id: string) => {
+    setSelectedTabId(id);
+  };
+  return (
+    <Container>
+      <Tabs
+        tabs={tabs}
+        selectedTabId={selectedTabId}
+        onTabClick={handleTabClick}
+      />
+    </Container>
+  );
 };
 
 export default MeetingDetail;
