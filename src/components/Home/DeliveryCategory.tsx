6469f3016000f8c7a4b48f6af18bd9f46b005729
JIN921 <wlslwlsl808@gmail.com>
2025-01-19 10:27:44 +0900

    design: #18 배달팟 카테고리 디자인 수정

--- src/components/Home/DeliveryCategory.tsx
+++ src/components/Home/DeliveryCategory.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -12,19 +13,43 @@ const ItemsContainer = styled.div`
   grid-template-columns: repeat(5, 1fr);
   gap: 8px;
   overflow: hidden;
+  padding-bottom: 5px;
   transition: max-height 0.3s ease-in-out;
 `;
 
-const Item = styled.div`
+const Item = styled.div<{ isSelected: boolean }>`
   display: flex;
   flex-direction: column;
   align-items: center;
   text-align: center;
+  cursor: pointer;
+  border-radius: 50%;
+  padding: 10px;
+  background-color: ${({ isSelected }) => (isSelected ? '#FBDED0' : 'white')};
+  border: 1px solid
+    ${({ isSelected, theme }) => (isSelected ? theme.COLORS.main : '#E0E0E0')};
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  transition:
+    background-color 0.3s ease,
+    border-color 0.3s ease,
+    box-shadow 0.3s ease;
+
+  &:hover {
+    border: 1px solid ${({ theme }) => theme.COLORS.main};
+  }
 `;
 
 const Icon = styled.img`
   width: 30px;
   height: 30px;
+`;
+
+const Label = styled.span<{ isSelected: boolean }>`
+  margin-top: 5px;
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ isSelected, theme }) =>
+    isSelected ? theme.COLORS.main : '#636363'};
 `;
 
 const items = [
@@ -41,17 +66,31 @@ const items = [
     name: '커피/차',
     icon: '/src/assets/images/delivery/ic_coffee.svg',
   },
-  { id: 10, name: '아시안', icon: '/src/assets/images/delivery/ic_asian.svg' },
+  {
+    id: 10,
+    name: '디저트',
+    icon: '/src/assets/images/delivery/ic_dessert.svg',
+  },
 ];
 
 const DeliveryCategory = () => {
+  const [selectedItem, setSelectedItem] = useState<number | null>(null);
+
+  const handleItemClick = (id: number) => {
+    setSelectedItem(id);
+  };
+
   return (
     <Wrapper>
       <ItemsContainer>
         {items.map((item) => (
-          <Item key={item.id}>
+          <Item
+            key={item.id}
+            isSelected={selectedItem === item.id}
+            onClick={() => handleItemClick(item.id)}
+          >
             <Icon src={item.icon} alt={item.name} />
-            <span>{item.name}</span>
+            <Label isSelected={selectedItem === item.id}>{item.name}</Label>
           </Item>
         ))}
       </ItemsContainer>
