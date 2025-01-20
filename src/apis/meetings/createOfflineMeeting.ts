8189640a0d957a63126291039f594596e9b18d46
dahhyeon <dahhyeon22@gmail.com>
2025-01-20 17:45:32 +0900

    feat:#22 api 요청 코드 추가

---
+++ src/apis/meetings/createOfflineMeeting.ts
@@ -0,0 +1,29 @@
+import defaultInstance from '@/apis/axiosInstance';
+
+const PATH = '/api/meetings';
+
+interface CreateMeetingRequest {
+  meetingName: string;
+  meetingDescription: string;
+  // "genderRestriction": "MALE",
+  isLimited: boolean;
+  maxParticipants: number | null;
+  meetingPlace: string;
+  meetingDate: string;
+  // "offlineMeetingCategory": string;         // 밥약인지 술약인지
+}
+
+export const createOfflineMeeting = async (
+  OfflineCreateData: CreateMeetingRequest
+) => {
+  try {
+    const response = await defaultInstance.post(
+      PATH + '/offline',
+      OfflineCreateData
+    );
+    return response.data;
+  } catch (error) {
+    console.error('오프라인 모임 생성 실패:  ', error);
+    throw error;
+  }
+};
