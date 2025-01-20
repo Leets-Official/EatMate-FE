3f7fb93689ba5f4b7fb74fd085805c9e06de02fb
dahhyeon <dahhyeon22@gmail.com>
2025-01-20 18:17:51 +0900

    feat:#22 input value들을 handleFormChange를 통해 처리

--- src/apis/meetings/createOfflineMeeting.ts
+++ src/apis/meetings/createOfflineMeeting.ts
@@ -17,10 +17,10 @@ export const createOfflineMeeting = async (
   OfflineCreateData: CreateMeetingRequest
 ) => {
   try {
-    const response = await defaultInstance.post(
-      PATH + '/offline',
-      OfflineCreateData
-    );
+    const response = await defaultInstance.post(PATH + '/offline', {
+      ...OfflineCreateData,
+      meetingDate: new Date(OfflineCreateData.meetingDate).toISOString(),
+    });
     return response.data;
   } catch (error) {
     console.error('오프라인 모임 생성 실패:  ', error);
--- src/pages/event/OfflineMeetingCreate.tsx
+++ src/pages/event/OfflineMeetingCreate.tsx
@@ -9,6 +9,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import MeetingTimeOption from '@/components/event/MeetingTimeOption';
+import { useInputHandler } from '@/hooks/useInputHandler';
 
 const ContentPadding = styled.div`
   padding: 20px 30px;
@@ -17,31 +18,59 @@ const ContentPadding = styled.div`
 const OfflineMeetingCreate: React.FC = () => {
   const nav = useNavigate();
 
-  const [genderSelected, setGenderSelected] = useState<boolean>(false);
-  const [errors, setErrors] = useState<{
-    gender?: Boolean;
-    storeName?: boolean;
-  }>({});
-  const [storeName, setStoreName] = useState('');
+  // const [genderSelected, setGenderSelected] = useState<boolean>(false);
+  // const [errors, setErrors] = useState<{
+  //   gender?: Boolean;
+  //   storeName?: boolean;
+  // }>({});
+  // const [storeName, setStoreName] = useState('');
 
-  const handleGenderChange = (value: string) => {
-    setGenderSelected(true);
-    setErrors((prev) => ({ ...prev, gender: false }));
-    console.log('선택된 성별 제한:', value);
-  };
+  // const handleGenderChange = (value: string) => {
+  //   setGenderSelected(true);
+  //   setErrors((prev) => ({ ...prev, gender: false }));
+  //   console.log('선택된 성별 제한:', value);
+  // };
 
-  const validateForm = () => {
-    const newErrors: { gender?: boolean; storeName?: boolean } = {};
-    if (!storeName.trim()) newErrors.storeName = true;
-    if (!genderSelected) newErrors.gender = true;
+  // const validateForm = () => {
+  //   const newErrors: { gender?: boolean; storeName?: boolean } = {};
+  //   if (!storeName.trim()) newErrors.storeName = true;
+  //   if (!genderSelected) newErrors.gender = true;
 
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
+  //   setErrors(newErrors);
+  //   return Object.keys(newErrors).length === 0;
+  // };
+
+  // const handleSubmit = () => {
+  //   if (validateForm()) {
+  //     console.log('모임이 정상적으로 생성되었습니다.');
+  //   }
+  // };
+
+  const { formData, errors, handleChange, validateForm } = useInputHandler({
+    meetingName: '',
+    meetingDescription: '',
+    isLimited: false,
+    maxParticipants: null,
+    meetingPlace: '',
+    meetingDate: '',
+    // gender:''
+    // offlineMeetingCategory: ""
+  });
+
+  const handleFormChange = (key: string, value: any) => {
+    handleChange(key, value);
+
+    // isLimited가 false이면 maxParticipants를 null 로 설정
+    if (key === 'isLimited' && !value) {
+      handleChange('maxParticipants', null);
+    }
   };
 
   const handleSubmit = () => {
-    if (validateForm()) {
-      console.log('모임이 정상적으로 생성되었습니다.');
+    if (validateForm(['meetingName', 'meetingDescription', 'meetingPlace'])) {
+      console.log('오프라인 모임이 정상적으로 생성되었습니다.', formData);
+    } else {
+      console.log('필수 입력값이 누락되었습니다.');
     }
   };
 
@@ -60,7 +89,8 @@ const OfflineMeetingCreate: React.FC = () => {
           as="textarea"
           placeholder="30자 이내"
           maxLength={30}
-          onChange={(e) => setMeetingName(e.target.value)}
+          onChange={(e) => handleFormChange('meetingName', e.target.value)}
+          hasError={errors.meetingName}
         />
         <Input
           label="모임 설명"
@@ -68,26 +98,36 @@ const OfflineMeetingCreate: React.FC = () => {
           placeholder="무엇을 하는 어떤 모임인가요?  100자 이내"
           maxLength={100}
           rows={4}
+          onChange={(e) =>
+            handleFormChange('meetingDescription', e.target.value)
+          }
+          hasError={errors.meetingDescription}
         />
+
         <BackgroundOption />
         <InputGuide
           message="모임 배경 화면에 들어갈 사진을 골라주세요."
           margin="15px"
         />
+
         <GenderOption
-          onChange={handleGenderChange}
+          // onChange={(e) => handleFormChange('gender', e.target.value)}
           showError={!!errors.gender}
         />
-        <ParticipantOption />
-        <MeetingTimeOption />
+        <ParticipantOption
+          onChange={(value) => handleFormChange('isLimited', value)}
+        />
+        <MeetingTimeOption
+          onChange={(value) => handleFormChange('meetingDate', value)}
+        />
         <div>
           <Input
             label="가게 이름"
             as="input"
             placeholder="가게명 입력"
             guideMessage="가게명과 지점명을 함께 입력해주세요"
-            onChange={(e) => setStoreName(e.target.value)}
-            hasError={errors.storeName}
+            onChange={(e) => handleFormChange('meetingPlace', e.target.value)}
+            hasError={errors.meetingPlace}
             errorMessage="다시 입력해주세요."
           />
         </div>
