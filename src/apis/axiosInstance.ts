924036b23a8101cf292840ff9f6e2d840449413c
dahhyeon <dahhyeon22@gmail.com>
2025-01-25 21:49:44 +0900

    feat:#30 axios interceptor로 에러 핸들링

--- src/apis/axiosInstance.ts
+++ src/apis/axiosInstance.ts
@@ -9,6 +9,7 @@ const defaultInstance = axios.create({
   },
 });
 
+// multipart/form-data 자동 설정
 defaultInstance.interceptors.request.use(
   (config) => {
     if (config.data instanceof FormData) {
@@ -18,6 +19,35 @@ defaultInstance.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
+  }
+);
+
+defaultInstance.interceptors.response.use(
+  (response) => {
+    return response; // 정상적인 응답
+  },
+  (error) => {
+    if (error.response) {
+      // 서버에서 반환된 응답이 있는 경우
+      console.error('API 요청 실패: ', {
+        status: error.response.status,
+        message: error.response.data?.message || '알 수 없는 에러 발생',
+      });
+
+      return Promise.reject(
+        new Error(error.response.data?.message || 'API 요청에 실패했습니다.')
+      );
+    }
+    // 서버에 도달하지 못한 경우
+    else if (error.request) {
+      console.error('서버 응답 없음: 네트워크 오류');
+      return Promise.reject(new Error('서버에 연결할 수 없습니다.'));
+    }
+    // 기타 에러 ( 요청 설정 문제 등 )
+    else {
+      console.error('요청 구성 오류: ', error.message);
+      return Promise.reject(new Error('요청 중 문제가 발생했습니다.'));
+    }
   }
 );
 
