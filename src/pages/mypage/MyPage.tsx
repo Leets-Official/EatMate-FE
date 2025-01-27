5939dbbd3e345e5756fad51e673098c27cfbc941
dahhyeon <dahhyeon22@gmail.com>
2025-01-27 17:19:16 +0900

    refactor:#36 style 분리

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -2,93 +2,8 @@ import ProfileIcon from '@/assets/images/ic_my_profile.svg';
 import BottomNavigation from '@/components/common/BottomNavi';
 import Button from '@/components/common/Button/Button';
 import { mypageMenuItems } from '@/constants/mypageMenuConstants';
-import {
-  flexAlignCenter,
-  flexCenter,
-  flexColumn,
-  flexColumnCenter,
-} from '@/styles/CommonStyle';
-import theme from '@/styles/theme';
+import * as S from '@/styles/mypage/mypage.styled';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-
-const ButtonContainer = styled.div`
-  padding: 20px;
-  ${flexCenter}
-  margin-bottom: 70px;
-`;
-
-const TitleContainer = styled.div`
-  padding: 20px 0 0 20px;
-`;
-const ProfileContainer = styled.div`
-  ${flexAlignCenter}
-  gap: 10px;
-  padding: 20px;
-`;
-
-const MeetingItems = styled.div`
-  ${flexColumnCenter}
-  gap: 5px;
-`;
-
-const MeetingContainer = styled.div`
-  ${flexCenter}
-  flex-direction: row;
-  gap: 70px;
-  padding: 15px;
-  margin-bottom: 10px;
-  /* background-color: ${({ theme }) => theme.COLORS.gray[50]}; */
-  background-color: #fcebcb;
-  width: 100%;
-`;
-const TextContainer = styled.div`
-  ${flexColumn}
-  gap:5px;
-`;
-
-const Text = styled.div<{
-  fontSize: keyof typeof theme.FONT_SIZE;
-  fontWeight?: keyof typeof theme.FONT_WEIGHT;
-  color?: 'gray';
-}>`
-  font-weight: ${({ theme, fontWeight = 'light' }) =>
-    theme.FONT_WEIGHT[fontWeight]};
-  font-size: ${({ theme, fontSize }) => theme.FONT_SIZE[fontSize]};
-  color: ${({ theme, color }) =>
-    color === 'gray' ? '#858585' : theme.COLORS.black};
-`;
-
-const MenuContainer = styled.div`
-  padding: 20px 40px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 30px;
-`;
-
-const MenuItem = styled.div`
-  ${flexAlignCenter}
-  gap: 20px;
-  cursor: pointer;
-
-  img {
-    width: 24px;
-    height: 24px;
-  }
-`;
-
-const SectionContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
-`;
-
-const Divider = styled.div`
-  height: 1px;
-  background-color: ${({ theme }) => theme.COLORS.gray[10]};
-  margin: 10px 0 -5px 0;
-`;
 
 const mockData = {
   name: '가천',
@@ -101,53 +16,53 @@ const MyPage: React.FC = () => {
   const nav = useNavigate();
   return (
     <div>
-      <TitleContainer>
-        <Text fontSize="xl" fontWeight="bold">
+      <S.TitleContainer>
+        <S.Text fontSize="xl" fontWeight="bold">
           마이페이지
-        </Text>
-      </TitleContainer>
-      <ProfileContainer>
+        </S.Text>
+      </S.TitleContainer>
+      <S.ProfileContainer>
         <img src={ProfileIcon} alt="profile" />
-        <TextContainer>
-          <Text fontSize="md">{mockData.name}</Text>
-          <Text fontSize="sm">{mockData.studentId}</Text>
-        </TextContainer>
-      </ProfileContainer>
-      <MeetingContainer>
-        <MeetingItems>
+        <S.TextContainer>
+          <S.Text fontSize="md">{mockData.name}</S.Text>
+          <S.Text fontSize="sm">{mockData.studentId}</S.Text>
+        </S.TextContainer>
+      </S.ProfileContainer>
+      <S.MeetingContainer>
+        <S.MeetingItems>
           <div>{mockData.create}</div>
-          <Text fontSize="sm" color="gray">
+          <S.Text fontSize="sm" color="gray">
             개설한 모임
-          </Text>
-        </MeetingItems>
-        <MeetingItems>
+          </S.Text>
+        </S.MeetingItems>
+        <S.MeetingItems>
           <div>{mockData.participation}</div>
-          <Text fontSize="sm" color="gray">
+          <S.Text fontSize="sm" color="gray">
             참여한 모임
-          </Text>
-        </MeetingItems>
-      </MeetingContainer>
-      <MenuContainer>
+          </S.Text>
+        </S.MeetingItems>
+      </S.MeetingContainer>
+      <S.MenuContainer>
         {mypageMenuItems.map((section, index) => (
-          <SectionContainer key={index}>
-            <Text fontSize="sm" color="gray">
+          <S.SectionContainer key={index}>
+            <S.Text fontSize="sm" color="gray">
               {section.category}
-            </Text>
+            </S.Text>
             {section.items.map((item, idx) => (
-              <MenuItem key={idx} onClick={() => nav(item.path)}>
+              <S.MenuItem key={idx} onClick={() => nav(item.path)}>
                 <img src={item.icon} alt={item.label} />
-                <Text fontSize="md">{item.label}</Text>
-              </MenuItem>
+                <S.Text fontSize="md">{item.label}</S.Text>
+              </S.MenuItem>
             ))}
-            {index !== mypageMenuItems.length - 1 && <Divider />}
-          </SectionContainer>
+            {index !== mypageMenuItems.length - 1 && <S.Divider />}
+          </S.SectionContainer>
         ))}
-      </MenuContainer>
-      <ButtonContainer>
+      </S.MenuContainer>
+      <S.ButtonContainer>
         <Button variant="primary" size="lg" rounded="sm">
           로그아웃
         </Button>
-      </ButtonContainer>
+      </S.ButtonContainer>
       <BottomNavigation />
     </div>
   );
---
+++ src/styles/mypage/mypage.styled.ts
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+import {
+  flexAlignCenter,
+  flexCenter,
+  flexColumn,
+  flexColumnCenter,
+} from '@/styles/CommonStyle';
+
+export const ButtonContainer = styled.div`
+  padding: 20px;
+  ${flexCenter}
+  margin-bottom: 70px;
+`;
+
+export const TitleContainer = styled.div`
+  padding: 20px 0 0 20px;
+`;
+
+export const ProfileContainer = styled.div`
+  ${flexAlignCenter}
+  gap: 10px;
+  padding: 20px;
+`;
+
+export const MeetingItems = styled.div`
+  ${flexColumnCenter}
+  gap: 5px;
+`;
+
+export const MeetingContainer = styled.div`
+  ${flexCenter}
+  flex-direction: row;
+  gap: 70px;
+  padding: 15px;
+  margin-bottom: 10px;
+  /* background-color: ${({ theme }) => theme.COLORS.gray[50]}; */
+  background-color: #fcebcb;
+  width: 100%;
+`;
+
+export const TextContainer = styled.div`
+  ${flexColumn}
+  gap:5px;
+`;
+
+export const Text = styled.div<{
+  fontSize: keyof typeof theme.FONT_SIZE;
+  fontWeight?: keyof typeof theme.FONT_WEIGHT;
+  color?: 'gray';
+}>`
+  font-weight: ${({ theme, fontWeight = 'light' }) =>
+    theme.FONT_WEIGHT[fontWeight]};
+  font-size: ${({ theme, fontSize }) => theme.FONT_SIZE[fontSize]};
+  color: ${({ theme, color }) =>
+    color === 'gray' ? '#858585' : theme.COLORS.black};
+`;
+
+export const MenuContainer = styled.div`
+  padding: 20px 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 30px;
+`;
+
+export const MenuItem = styled.div`
+  ${flexAlignCenter}
+  gap: 20px;
+  cursor: pointer;
+
+  img {
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+export const SectionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+export const Divider = styled.div`
+  height: 1px;
+  background-color: ${({ theme }) => theme.COLORS.gray[10]};
+  margin: 10px 0 -5px 0;
+`;
