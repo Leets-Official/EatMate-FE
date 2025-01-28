1f16eaabb9e021dc3c76e42c5193a4c17766672d
dahhyeon <dahhyeon22@gmail.com>
2025-01-28 16:12:46 +0900

    refactor:#36 절대경로로 수정

--- src/styles/mypage/userInfoEdit.styled.ts
+++ src/styles/mypage/userInfoEdit.styled.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { flexAlignCenter, flexCenter, flexColumn } from '../CommonStyle';
+import { flexAlignCenter, flexCenter, flexColumn } from '@/styles/CommonStyle';
 
 export const Container = styled.div`
   ${flexColumn}
