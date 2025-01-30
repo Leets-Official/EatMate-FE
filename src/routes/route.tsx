9e3d971ab7e22ef6f7ce807e24599a1f642ad31c
dahhyeon <dahhyeon22@gmail.com>
2025-01-30 23:10:28 +0900

    fix:#42 파일 경로 오류 수정

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -25,7 +25,7 @@ import GenderStep from '@/components/SignUp/GenderStep';
 import SignupSuccess from '@/pages/signup/SignupSuccess';
 import Splash from '@/pages/Splash';
 import NotFound from '@/pages/Not-found';
-import AuthGuard from '@/pages/AuthGuard';
+import AuthGuard from '@/components/common/AuthGuard';
 
 const router = createBrowserRouter([
   {
