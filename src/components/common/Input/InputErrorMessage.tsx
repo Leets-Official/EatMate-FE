9bea37390ce6aaa65680272bd87d9cce03034cfd
dahhyeon <dahhyeon22@gmail.com>
2025-01-30 23:11:06 +0900

    refactor:#42 불필요한 코드 제거

--- src/components/common/Input/InputErrorMessage.tsx
+++ src/components/common/Input/InputErrorMessage.tsx
@@ -1,4 +1,3 @@
-// import errorCheck from '@/assets/images/error_check.svg';
 import { ErrorContainer } from './styles';
 
 interface ErrorMessageProps {
@@ -8,20 +7,9 @@ interface ErrorMessageProps {
 const InputErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {
   return (
     <ErrorContainer>
-      {/* <img src={errorCheck} alt="check-icon" /> */}
       <span>{message}</span>
     </ErrorContainer>
   );
 };
 
 export default InputErrorMessage;
-
-{
-  /*
-  refactor ( props 옵션 추가 ) :
-       체크 svg 선택 여부 
-        // 체크 svg 색깔 
-        // 텍스트 색깔  
-        텍스트 크기
-  */
-}
