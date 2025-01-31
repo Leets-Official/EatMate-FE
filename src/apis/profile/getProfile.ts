1fee717a881fe71f8ea8199453d5f442bed30f68
dahhyeon <dahhyeon22@gmail.com>
2025-01-31 18:46:41 +0900

    feat:#46 api 연결 코드 추가

---
+++ src/apis/profile/getProfile.ts
@@ -0,0 +1,23 @@
+import defaultInstance from '../axiosInstance';
+
+const PATH = '/api/profile';
+
+export interface ProfileData {
+  email: string;
+  nickname: string;
+  studentNumber: number | null;
+  mbti: string | null;
+  year: number | null;
+  month: number | null;
+  day: number | null;
+  gender: string;
+  phoneNumber: string;
+  profileImageUrl?: string;
+}
+
+export const getProfileInfo = async (): Promise<ProfileData> => {
+  const response = await defaultInstance.get<{ result: ProfileData }>(
+    PATH + '/myinfo'
+  );
+  return response.data.result;
+};
