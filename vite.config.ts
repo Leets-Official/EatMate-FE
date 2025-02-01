6658c0dbbd7c1e7c8e9a9a2a209f5efb4b10d784
dahhyeon <dahhyeon22@gmail.com>
2025-02-01 22:27:58 +0900

    Revert "hotfix:vite.config 파일 base url 설정"
    This reverts commit 3e711f9dd664f3cbd31d4e7647e9f05211de3e32.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  base: '/',
   plugins: [react(), svgr()],
   server: {
     port: 3000,
