4a228ac8f03790587c272d5b819ca1d02dff40da
dahhyeon <dahhyeon22@gmail.com>
2025-02-03 00:29:56 +0900

    feat:#56 스플래쉬 화면 애니메이션 추가

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -4,10 +4,11 @@ import { useNavigate } from 'react-router-dom';
 import LogoIcon from '@/assets/images/ic_eatmate_white_logo.svg?react';
 import styled from 'styled-components';
 import { flexColumnCenter } from '@/styles/CommonStyle';
+import { motion } from 'framer-motion';
 
-const Flex = styled.div`
+const Flex = styled(motion.div)`
   ${flexColumnCenter}
-  gap:10px;
+  gap: 10px;
   width: 100%;
   height: 100vh;
   background-color: ${({ theme }) => theme.COLORS.main};
@@ -24,13 +25,22 @@ const Splash: React.FC = () => {
     return () => clearTimeout(timer);
   }, [nav]);
 
-  // 애니메이션 나중에 추가 예정
   return (
-    <Flex>
+    <Flex
+      initial={{ opacity: 1 }}
+      animate={{ opacity: 0 }}
+      transition={{ duration: 2.5, ease: 'easeInOut' }}
+    >
       <Text fontSize="smMd" color="white">
         모두를 잇다
       </Text>
+      {/* <motion.div
+        initial={{ fill: '#FFFFFF' }}
+        animate={{ fill: '#F3AA24' }}
+        transition={{ duration: 3, ease: 'easeOut' }}
+      > */}
       <LogoIcon />
+      {/* </motion.div> */}
     </Flex>
   );
 };
