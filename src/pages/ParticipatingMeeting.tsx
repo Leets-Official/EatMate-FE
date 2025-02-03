118c9b5ca7e41daba02d06967a72d66b23102d84
JIN921 <wlslwlsl808@gmail.com>
2025-02-04 03:47:46 +0900

    fix: #59 data 넣어주는 방식으로 수정

--- src/pages/ParticipatingMeeting.tsx
+++ src/pages/ParticipatingMeeting.tsx
@@ -1,27 +1,23 @@
-import {
-  flexAlignCenter,
-  flexColumn,
-  flexColumnCenter,
-} from '@/styles/CommonStyle';
-import { MeetingItems } from '@/styles/mypage/mypage.styled';
+import React from 'react';
 import styled from 'styled-components';
 import BeverageIcon from '@/assets/images/ic_beverage.svg';
 import MealIcon from '@/assets/images/ic_meal.svg';
 import DeliveryIcon from '@/assets/images/ic_delivery.svg';
 import LocoIcon from '@/assets/images/EatMate_main_Logo.svg';
+import { formatTimeWithMeridiem } from '@/utils/dateUtils';
+
 const Container = styled.div`
   margin: 0 auto;
   max-width: 390px;
   position: relative;
-  padding-top: 25px;
   background-color: #fdeed3;
 `;
 
-// 배너 스타일
 const Banner = styled.div`
   padding: 20px;
   text-align: center;
-  ${flexColumn}
+  display: flex;
+  flex-direction: column;
   align-items: center;
 `;
 
@@ -31,75 +27,112 @@ const LogoContainer = styled.div`
   justify-content: flex-start;
   align-self: flex-start;
 `;
+
 const Logo = styled.img`
   width: 72px;
   height: 28px;
 `;
+
 const BannerImage = styled.img`
   width: 100px;
   height: auto;
 `;
+
 const BannerTitle = styled.div`
-  font-size: ${({ theme }) => theme.FONT_SIZE.lg};
+  font-size: 24px;
   margin-top: 10px;
-  font-weight: ${({ theme }) => theme.FONT_WEIGHT.bold};
+  font-weight: bold;
 `;
+
 const Description = styled.div`
   color: #636363;
-  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
+  font-size: 14px;
   margin-top: 10px;
   word-wrap: break-word;
   white-space: normal;
 `;
+
 const BadgeContainer = styled.div`
   display: flex;
   flex-direction: row;
   gap: 10px;
   margin-top: 10px;
 `;
+
 const Badge = styled.div`
-  background-color: ${({ theme }) => theme.COLORS.white};
+  background-color: white;
   color: #f3aa24;
-  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
-  font-weight: ${({ theme }) => theme.FONT_WEIGHT.light};
+  font-size: 12px;
+  font-weight: light;
   padding: 3px 8px;
   border-radius: 5px;
-  ${flexAlignCenter}
+  display: flex;
+  align-items: center;
   white-space: nowrap;
   z-index: 10;
 `;
-
-// 참가 모임 스타일
-const ParticipatingMeetingList = styled.div`
-  background-color: ${({ theme }) => theme.COLORS.white};
-  border-top-left-radius: 25px;
-  border-top-right-radius: 25px;
-  ${flexColumnCenter}
-  gap: 20px;
-`;
+const mockData = {
+  meetingType: 'delivery',
+  id: 1,
+  meetingName: '칵테일 마시기',
+  meetingStatus: 'ACTIVE',
+  maxParticipants: 10,
+  offlineMeetingCategory: 'BEVERAGE',
+  createdAt: '2025-02-03T16:02:30.622Z',
+  location: '가천 칵테일 바',
+  dueDateTime: '2025-02-03T18:20:00.000Z',
+  participantCount: 3,
+  isOwn: true,
+};
 
 const ParticipatingMeeting = () => {
+  const getMeetingDetails = () => {
+    const category = mockData.offlineMeetingCategory;
+    let badge = '';
+    let iconSrc = '';
+
+    switch (mockData.meetingType) {
+      case 'delivery':
+        badge = '배달팟';
+        iconSrc = DeliveryIcon;
+        break;
+      case 'offline':
+        if (category === 'MEAL') {
+          badge = '밥약';
+          iconSrc = MealIcon;
+        } else if (category === 'BEVERAGE') {
+          badge = '술약';
+          iconSrc = BeverageIcon;
+        }
+        break;
+      default:
+        iconSrc = '';
+        break;
+    }
+
+    return { badge, iconSrc };
+  };
+
+  const { badge, iconSrc } = getMeetingDetails();
+
   return (
     <Container>
       <Banner>
         <LogoContainer>
           <Logo src={LocoIcon} alt="로고" />
         </LogoContainer>
-        <BannerImage src={BeverageIcon} alt="맥주 잔" />
+        <BannerImage src={iconSrc} alt={`${badge} 이미지`} />
         <BadgeContainer>
-          <Badge>내가 주최한 모임</Badge>
-          <Badge>술약</Badge>
+          {mockData.isOwn && <Badge>내가 주최한 모임</Badge>}
+          {badge && <Badge>{badge}</Badge>}
         </BadgeContainer>
-
-        <BannerTitle>안녕하세요 가천님</BannerTitle>
+        <BannerTitle>안녕하세요 00님</BannerTitle>
         <Description>
-          오후 6시 10분에 마라탕집 맛있겠어요점에서
-          <br /> 마라탕 맛나게 냠냠냠 ٩( ᐛ )و 모임 약속이 있어요
+          {`${formatTimeWithMeridiem(mockData.dueDateTime)}`}에{' '}
+          {`${mockData.location}`}에서 <br />
+          {`${mockData.meetingName}`} 약속이 있어요
         </Description>
       </Banner>
-      <ParticipatingMeetingList>
-        <MeetingItems>dd</MeetingItems>
-      </ParticipatingMeetingList>
     </Container>
   );
 };
