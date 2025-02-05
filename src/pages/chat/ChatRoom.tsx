9e1ad3039528c1e6ec821e384744a0d16dd5a4c4
JIN921 <wlslwlsl808@gmail.com>
2025-02-06 01:50:28 +0900

    refactor: #52 채팅 스타일 분리

--- src/pages/chat/ChatRoom.tsx
+++ src/pages/chat/ChatRoom.tsx
@@ -1,123 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import sendIcon from '@/assets/images/ic_backImg_default1.svg';
 import Header from '@/components/common/Header/Header';
 import ProfileIcon from '@/assets/images/ic_participant1.svg';
 import Notice from '@/components/chat/Notice';
 import ChatModal from '@/components/common/Modal/ChatModal';
 import ChatExitModal from '@/components/common/Modal/ChatExitModal';
-import { flexColumn } from '@/styles/CommonStyle';
 import useWebSocket from '@/hooks/useWebSocket';
 import { formatTime, formatTimeWithMeridiem } from '@/utils/dateUtils';
 import { ChatRoomDetails, getChatApi } from '@/apis/chat/getChatData';
 import Loading from '@/components/common/Loading';
 import { getUserProfileInfo, MemberData } from '@/apis/profile/getProfile';
-
-const ChatContainer = styled.div`
-  ${flexColumn}
-  height: 100vh;
-  justify-content: space-between;
-`;
-
-const MessagesList = styled.div`
-  flex-grow: 1;
-  overflow-y: auto;
-  padding: 0 10px;
-`;
-
-const Message = styled.div<{ isMine: boolean }>`
-  ${flexColumn}
-  align-items: ${({ isMine }) => (isMine ? 'flex-end' : 'flex-start')};
-  margin: 10px;
-`;
-
-const MessageBox = styled.div<{ isMine: boolean }>`
-  max-width: 70%;
-  padding: 10px;
-  border-radius: ${({ isMine }) =>
-    isMine ? '15px 0 15px 15px' : '0 15px 15px 15px'};
-  background-color: ${({ isMine, theme }) =>
-    isMine ? theme.COLORS.main : '#DDDDDD'};
-  color: ${({ isMine, theme }) =>
-    isMine ? theme.COLORS.white : theme.COLORS.black};
-  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
-`;
-
-const MessageContent = styled.div<{ isMine: boolean }>`
-  display: flex;
-  flex-direction: row;
-  justify-content: ${({ isMine }) => (isMine ? 'flex-end' : 'flex-start')};
-  align-items: center;
-  width: 100%;
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  height: 40px;
-  align-items: center;
-  padding: 0 12px;
-  border-radius: 20px;
-  background-color: #eeeeee;
-  margin: 0 10px 30px 10px;
-`;
-
-const Input = styled.input`
-  flex: 1;
-  height: 100%;
-  border: none;
-  outline: none;
-  background-color: transparent;
-  color: balck;
-  font-size: 14px;
-
-  &::placeholder {
-    color: #b6b6b6;
-  }
-`;
-
-const SendButton = styled.button`
-  background: url(${sendIcon}) no-repeat center;
-  border: none;
-  width: 27px;
-  height: 27px;
-  border-radius: 50%;
-  cursor: pointer;
-`;
-
-const ProfileContainer = styled.div<{ isMine: boolean }>`
-  display: ${({ isMine }) => (isMine ? 'none' : 'flex')};
-  align-items: center;
-  margin-bottom: 5px;
-`;
-
-const ProfileText = styled.div`
-  margin-left: 8px;
-  font-size: ${({ theme }) => theme.FONT_SIZE.smMd};
-`;
-
-const ProfileImg = styled.img`
-  border: none;
-  width: 31px;
-  height: 31px;
-  border-radius: 50%;
-  cursor: pointer;
-`;
-const TimeStamp = styled.span<{ isMine: boolean }>`
-  font-size: ${({ theme }) => theme.FONT_SIZE.xs};
-  color: #bfbfbf;
-  padding: 5px;
-  align-self: end;
-`;
-
-const DateContainer = styled.div`
-  background-color: #eeeeee;
-  color: #bfbfbf;
-  font-size: ${({ theme }) => theme.FONT_SIZE.sm};
-  padding: 8px 16px;
-  border-radius: 20px;
-  width: fit-content;
-  margin: 0px auto;
-`;
+import * as S from '@/styles/chat/ChatRoom.styled';
 
 interface ChatMessage {
   senderId: number;
@@ -147,10 +39,10 @@ const ChatRoom = () => {
     const msUntilMidnight = nextMidnight.getTime() - now.getTime();
 
     const timer = setTimeout(() => {
-      setToday(new Date()); // 자정이 되면 날짜 업데이트
+      setToday(new Date());
     }, msUntilMidnight);
 
-    return () => clearTimeout(timer); // 컴포넌트 unmount 시 타이머 제거
+    return () => clearTimeout(timer);
   }, [today]);
 
   // 날짜를 "YYYY년 MM월 DD일 (D)" 형식으로 포맷
@@ -266,7 +158,7 @@ const ChatRoom = () => {
   console.log('정보', getUserProfileInfo(myId));
 
   return (
-    <ChatContainer>
+    <S.ChatContainer>
       <Header
         title={chatRoomDetails.meetingName}
         showBackButton={true}
@@ -288,47 +180,47 @@ const ChatRoom = () => {
         />
       )}
 
-      <DateContainer>{formatDate(today)}</DateContainer>
-      <MessagesList>
+      <S.DateContainer>{formatDate(today)}</S.DateContainer>
+      <S.MessagesList>
         {messages?.chattingMessage
           ?.filter((msg) => msg.content !== null)
           .map((msg, index) => {
             const userProfile = userProfiles[msg.senderId];
             return (
-              <Message key={index} isMine={msg.senderId === myId}>
-                <ProfileContainer isMine={msg.senderId === myId}>
-                  <ProfileImg
+              <S.Message key={index} isMine={msg.senderId === myId}>
+                <S.ProfileContainer isMine={msg.senderId === myId}>
+                  <S.ProfileImg
                     src={userProfile?.profileImageUrl || ProfileIcon}
                     alt={userProfile?.nickname || 'User Image'}
                   />
-                  <ProfileText>
+                  <S.ProfileText>
                     {userProfile
                       ? `${userProfile.nickname} | ${userProfile.mbti}`
                       : 'Loading...'}
-                  </ProfileText>
-                </ProfileContainer>
-                <MessageContent isMine={msg.senderId === myId}>
-                  <TimeStamp isMine={msg.senderId === myId}>
+                  </S.ProfileText>
+                </S.ProfileContainer>
+                <S.MessageContent isMine={msg.senderId === myId}>
+                  <S.TimeStamp isMine={msg.senderId === myId}>
                     {formatTime(msg.regDate)}
-                  </TimeStamp>
-                  <MessageBox isMine={msg.senderId === myId}>
+                  </S.TimeStamp>
+                  <S.MessageBox isMine={msg.senderId === myId}>
                     {msg.content}
-                  </MessageBox>
-                </MessageContent>
-              </Message>
+                  </S.MessageBox>
+                </S.MessageContent>
+              </S.Message>
             );
           })}
-      </MessagesList>
+      </S.MessagesList>
 
-      <InputContainer>
-        <Input
+      <S.InputContainer>
+        <S.Input
           value={inputText}
           onChange={handleInput}
           onKeyPress={handleKeyPress}
           placeholder="메시지 입력"
         />
-        <SendButton onClick={handleSendMessage} />
-      </InputContainer>
+        <S.SendButton onClick={handleSendMessage} />
+      </S.InputContainer>
       {isChatModalOpen && (
         <ChatModal
           title={chatRoomDetails.meetingName}
@@ -340,7 +232,7 @@ const ChatRoom = () => {
       {isChatExitModalOpen && (
         <ChatExitModal roomId={roomId} onClose={closeChatExitModal} />
       )}
-    </ChatContainer>
+    </S.ChatContainer>
   );
 };
 
