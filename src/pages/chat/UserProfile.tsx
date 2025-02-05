3a236fd39274d976312b7907ace6054d88f48e0f
JIN921 <wlslwlsl808@gmail.com>
2025-02-06 07:52:19 +0900

    fix: 빌드 에러 수정

--- src/pages/chat/UserProfile.tsx
+++ src/pages/chat/UserProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Header from '@/components/common/Header/Header';
 import BlockIcon from '@/assets/images/ic_block.png';
